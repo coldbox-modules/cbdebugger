d2f5954aa4679ab5ae415aff416522224690dee1
Luis Majano <lmajano@gmail.com>
2022-04-19 16:36:05 -0500

    final removal of jquery, it's no more!

--- resources/assets/js/cbdebugger.js
+++ resources/assets/js/cbdebugger.js
@@ -1,4 +1,3 @@
-import $cb from "jquery";
 import Alpine from "alpinejs";
 // Using common js due to NOT being on webpack5, the esm was giving us issues
 // Once we update to elixir 4, try it again
@@ -9,7 +8,7 @@ Alpine.plugin( morph );
 Alpine.start();
 
 /**
- * Listen to dom load
+ * Listen to dom load and attach
  */
 window.onload = ()=> console.log( "ColdBox Debugger Loaded!" );
 
@@ -25,18 +24,9 @@ window.cbdScrollTo = function( id ){
 };
 
 
-/****************** MIGRATE BELOW ****************/
-
-/**
- * Toggle on/off debug content views
- * @param {*} divid The div id to toggle on and off
- */
-window.cbdToggle = function( divid ){
-	$cb( "#" + divid ).slideToggle();
-};
-
 /**
  * Open a new window
+ *
  * @param {*} mypage the target page
  * @param {*} myname the target name
  * @param {*} w width
@@ -44,83 +34,21 @@ window.cbdToggle = function( divid ){
  * @param {*} features features to add
  */
 window.cbdOpenWindow = function( mypage, myname, w, h, features ) {
-	if ( screen.width ){
-		var winl = ( screen.width-w )/2;
-		var wint = ( screen.height-h )/2;
-	}
-	else {
-		winl = 0;wint =0;
-	}
+	let winl = screen.width ? ( screen.width - w ) / 2 : 0;
+	let wint = screen.width ? ( screen.height - h ) / 2 : 0;
 	if ( winl < 0 ) winl = 0;
 	if ( wint < 0 ) wint = 0;
-
-	var settings = "height=" + h + ",";
-	settings += "width=" + w + ",";
-	settings += "top=" + wint + ",";
-	settings += "left=" + winl + ",";
-	settings += features;
-	win = window.open( mypage,myname,settings );
+	let win = window.open(
+		mypage,
+		myname,
+		`height=${h},width=${w},top=${wint},left=${winl}+${features}`
+	);
 	win.window.focus();
 };
 
 /**
- * Show group queries and hide timeline queries
- */
-window.cbdShowGroupedQueries = function() {
-	// Watch out for double toggles
-	if ( $cb( "#cbdGroupedQueries" ).css( "display" ) == "block" ){
-		return;
-	}
-	$cb( "#cbdButtonGroupedQueries" ).addClass( "cbd-selected" );
-	$cb( "#cbdButtonTimelineQueries" ).removeClass( "cbd-selected" );
-	$cb( "#cbdTimelineQueries" ).slideUp();
-	$cb( "#cbdGroupedQueries" ).slideDown();
-};
-
-/**
- * Show timeline queries and hide group queries
- */
-window.cbdShowTimelineQueries = function() {
-	// Watch out for double toggles
-	if ( $cb( "#cbdTimelineQueries" ).css( "display" ) == "block" ){
-		return;
-	}
-	$cb( "#cbdButtonTimelineQueries" ).addClass( "cbd-selected" );
-	$cb( "#cbdButtonGroupedQueries" ).removeClass( "cbd-selected" );
-	$cb( "#cbdTimelineQueries" ).slideDown();
-	$cb( "#cbdGroupedQueries" ).slideUp();
-};
-
-/**
- * Show group queries and hide timeline queries for orm
- */
-window.cbdShowGroupedOrmQueries = function() {
-	// Watch out for double toggles
-	if ( $cb( "#cbdGroupedOrmQueries" ).css( "display" ) == "block" ){
-		return;
-	}
-	$cb( "#cbdButtonGroupedOrmQueries" ).addClass( "cbd-selected" );
-	$cb( "#cbdButtonTimelineOrmQueries" ).removeClass( "cbd-selected" );
-	$cb( "#cbdTimelineOrmQueries" ).slideUp();
-	$cb( "#cbdGroupedOrmQueries" ).slideDown();
-};
-
-/**
- * Show timeline queries and hide group queries for orm
- */
-window.cbdShowTimelineOrmQueries = function() {
-	// Watch out for double toggles
-	if ( $cb( "#timelineQueries" ).css( "display" ) == "block" ){
-		return;
-	}
-	$cb( "#cbdButtonTimelineOrmQueries" ).addClass( "cbd-selected" );
-	$cb( "#cbdButtonGroupedOrmQueries" ).removeClass( "cbd-selected" );
-	$cb( "#cbdTimelineOrmQueries" ).slideDown();
-	$cb( "#cbdGroupedOrmQueries" ).slideUp();
-};
-
-/**
  * Copy a div's code to the clipboard
+ *
  * @param {*} id The id of the element's content to copy to the clipboard
  */
 window.copyToClipboard = function( id ) {
